6f9c91a9f03815fced96fcfb140e236c8044887a
it19075136 <it19075136@my.sliit.lk>
2022-03-23 00:30:06 +0530

    User management and welcome screen

---
+++ lib/pages/login/login.dart
@@ -0,0 +1,137 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:spark_tv_shows/constants.dart';
+import 'package:spark_tv_shows/pages/services/auth/firebaseAuth.dart';
+import 'package:spark_tv_shows/pages/signUp/register.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _key = GlobalKey<FormState>();
+
+  final FirebaseAuthentication _auth = FirebaseAuthentication();
+
+  TextEditingController _emailContoller = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: kPrimaryLightColor,
+        child: Center(
+          child: Form(
+            key: _key,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: kPrimaryColor,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 5),
+                TextButton(
+                    child: Text('Not registerd? Sign up'),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        CupertinoPageRoute(
+                          fullscreenDialog: true,
+                          builder: (context) => Register(),
+                        ),
+                      );
+                    },
+                    style: TextButton.styleFrom(
+                        primary: kPrimaryColor,
+                        textStyle: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                            decoration:   TextDecoration.underline
+                        )
+                    )
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 30),
+                      TextFormField(
+                        controller: _emailContoller,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email cannot be empty';
+                          } else
+                            return null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: kPrimaryColor)),
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password cannot be empty';
+                          } else
+                            return null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: kPrimaryColor)),
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            child: Text('Login'),
+                            onPressed: () {
+                              if (_key.currentState!.validate()) {
+                                login();
+                              }
+                            },
+                            style: TextButton.styleFrom(
+                                primary: kPrimaryLightColor,
+                                backgroundColor: kPrimaryColor
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() async {
+    dynamic result = await _auth.login(_emailContoller.text, _passwordController.text);
+    if(result == null){
+      print("Failed to login");
+    }
+    else {
+      print(result.toString());
+      _emailContoller.clear();
+      _passwordController.clear();
+      Navigator.pushNamed(context, '/welcome');
+    }
+  }
+}
---
+++ lib/pages/signUp/register.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:spark_tv_shows/constants.dart';
+import 'package:spark_tv_shows/pages/login/login.dart';
+import 'package:spark_tv_shows/pages/services/auth/firebaseAuth.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+    final _key = GlobalKey<FormState>();
+
+    final FirebaseAuthentication _auth = FirebaseAuthentication();
+
+    final TextEditingController _nameController = TextEditingController();
+    final TextEditingController _emailContoller = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _phoneController = TextEditingController();
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Container(
+          color: kPrimaryLightColor,
+          child: Center(
+            child: Form(
+              key: _key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Register',
+                    style: TextStyle(
+                      color: kPrimaryColor,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  TextButton(
+                      child: const Text('Have an account? Sign in'),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          CupertinoPageRoute(
+                            fullscreenDialog: true,
+                            builder: (context) => const Login(),
+                          ),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                          primary: kPrimaryColor,
+                          textStyle: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                            decoration:   TextDecoration.underline
+                          )
+                      )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _nameController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Name cannot be empty';
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: const InputDecoration(
+                              labelText: 'Name',
+                              labelStyle: TextStyle(
+                                color: kPrimaryColor,
+                              )),
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        TextFormField(
+                          controller: _emailContoller,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Email cannot be empty';
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: const InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: kPrimaryColor)),
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Password cannot be empty';
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: const InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(color: kPrimaryColor)),
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        TextFormField(
+                          controller: _phoneController,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Phone cannot be empty';
+                            } else {
+                              return null;
+                            }
+                          },
+                          decoration: const InputDecoration(
+                              labelText: 'Phone',
+                              labelStyle: TextStyle(color: kPrimaryColor)),
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextButton(
+                              child: const Text('Sign Up'),
+                              onPressed: () {
+                                if (_key.currentState!.validate()) {
+                                  insertUser();
+                                }
+                              },
+                              style: TextButton.styleFrom(
+                                  primary: kPrimaryLightColor,
+                                  backgroundColor: kPrimaryColor
+                              ),
+                            ),
+                            TextButton(
+                              child: const Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                               },
+                              style: TextButton.styleFrom(
+                                  primary: kPrimaryLightColor,
+                                  backgroundColor: kPrimaryColor
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+  void insertUser() async {
+      dynamic result = await _auth.insertUser(_nameController.text, _emailContoller.text, _passwordController.text, _phoneController.text, 'user');
+      if(result == null) {
+        print("Invalid credentials");
+      }
+      else {
+        print(result.toString());
+        _nameController.clear();
+        _passwordController.clear();
+        _emailContoller.clear();
+        Navigator.pop(context);
+      }
+  }
+
+}
+
+
